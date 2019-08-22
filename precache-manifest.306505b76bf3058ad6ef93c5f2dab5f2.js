b3602debdd672b17095b5162a7315e45e569e2bb
Wilde <31402134+asd3er@users.noreply.github.com>
2019-08-21 21:08:25 -0700

    Updates

---
+++ precache-manifest.306505b76bf3058ad6ef93c5f2dab5f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c0c95c6e53fe6a516b22d6c484216b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad97e7b8573b088f8dfe",
+    "url": "/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "52ea09646a452dd4074d",
+    "url": "/static/css/main.d8e4ccde.chunk.css"
+  },
+  {
+    "revision": "ad97e7b8573b088f8dfe",
+    "url": "/static/js/2.541164eb.chunk.js"
+  },
+  {
+    "revision": "52ea09646a452dd4074d",
+    "url": "/static/js/main.d43add4b.chunk.js"
+  },
+  {
+    "revision": "b0a546bcc125fb21d6be",
+    "url": "/static/js/runtime~main.75271b63.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
